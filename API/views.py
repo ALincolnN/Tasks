f2e95ffacb6c44b0e2343512b9cb004f05bc28ed
LincolnNjogu <njoguabraham2@gmail.com>
2022-08-29 15:03:54 +0300

    1. Added the create function for the tasks i.e. The form is now able to get the user input and save it onto the database. 2. Added the functionality of clearing out the text field after a task is submitted.

--- API/views.py
+++ API/views.py
@@ -26,7 +26,7 @@ def apiGuide(request):
 @api_view(['GET'])
 def tasklist(request):
 
-    tasks=Task.objects.all()
+    tasks=Task.objects.all().order_by('-id')
     serializer=TaskSerializer(tasks,many=True)
     return Response(serializer.data)
 
